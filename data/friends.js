eff90a191af8ae81048a0294f3de04bf91f16b0e
Salma Manda <salmamanda03@gmail.com>
2025-04-09 14:25:32 +0700

    fix: data json structure

---
+++ data/friends.js
@@ -0,0 +1,34 @@
+const friends = [
+	{
+		key: "alex",
+		givenName: "Alex",
+		familyName: "",
+		image: { uri: "https://randomuser.me/api/portraits/men/32.jpg" },
+	},
+	{
+		key: "jenny",
+		givenName: "Jenny",
+		familyName: "",
+		image: { uri: "https://randomuser.me/api/portraits/women/65.jpg" },
+	},
+	{
+		key: "sam",
+		givenName: "Sam",
+		familyName: "",
+		image: { uri: "https://randomuser.me/api/portraits/men/75.jpg" },
+	},
+	{
+		key: "taylor",
+		givenName: "Taylor",
+		familyName: "",
+		image: { uri: "https://randomuser.me/api/portraits/women/22.jpg" },
+	},
+	{
+		key: "james",
+		givenName: "James",
+		familyName: "",
+		image: { uri: "https://randomuser.me/api/portraits/men/12.jpg" },
+	},
+];
+
+export default friends;
